c5d53db66730cda7678eafec7e43c8d31d9cedb3
averyc1876 <averyc2876@gmail.com>
2025-09-02 14:00:24 +0100

    Add project files.

---
+++ GrowingWildGrass/GrowingWildGrass/GrowingWildGrassModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace GrowingWildGrass
+{
+    public class GrowingWildGrassModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+        }
+
+    }
+}
